90065b3430f25e05dd6ecac89737e5e8d46c73ae
protap55 <protapdutta500@gmail.com>
2026-01-29 00:09:02 +0600

    useauth done

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../Firebase/Firebase.init";
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AuthProvider = ({ children }) => {
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = {
+    registerUser,
+    signInUser,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/Router.jsx";
 import { RouterProvider } from "react-router/dom";
+import AuthProvider from "./Context/AuthContext/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="max-w-7xl mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>,
 );
